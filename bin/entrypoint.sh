cf8bd1c33501e51dd0cc3bf68beaf89844733b73
Henk Kraal <hkraal@users.noreply.github.com>
2024-08-18 12:02:43 +0200

    Use setpriv to start nagios

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -6,6 +6,4 @@ apachectl start
 
 chgrp www-data /usr/local/nagios/var/rw
 
-htpasswd -bc /usr/local/nagios/etc/htpasswd.users $NAGIOS_USER $NAGIOS_PASSWORD
-
-/usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg
+setpriv --reuid=nagios --regid=nagios --init-groups /usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg

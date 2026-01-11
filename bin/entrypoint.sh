9e5a98300d5e5f10ae65d2fa83ab57f94aaea6bf
Henk Kraal <hkraal@users.noreply.github.com>
2026-01-11 11:44:00 +0100

    fix: permission denied on fifo file

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 apachectl start
 
-chgrp www-data /usr/local/nagios/var/rw
+chown nagios:www-data /usr/local/nagios/var/rw
 
 setpriv --reuid=nagios --regid=nagios --init-groups /usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg
